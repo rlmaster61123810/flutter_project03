f731e6d439f81de89a6282db7f5be7db39870218
The new begin <42103767+rlmaster61123810@users.noreply.github.com>
2021-09-08 12:17:54 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,13 @@ void main() {
   runApp(MyApp());
 }
 
+class pageArguments {
+  final String title;
+  final String message;
+  pageArguments(this.title, this.message);
+}
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,58 +18,140 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
+      // home: FirstPage(),
       routes: {
-        '/': (context) => const FistPage(),
-        '/Second': (context) => const SecondPage(),
+        '/': (context) => const FirstPage(),
+        // '/second': (context) => const SecondPage(),
+        SecondPage.routeName: (context) => const SecondPage(),
+        ThirdPage.routeName: (context) => const ThirdPage(),
       },
     );
   }
 }
 
-class FistPage extends StatelessWidget {
-  const FistPage({Key? key}) : super(key: key);
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Fist Page'),
+      appBar: AppBar(
+        title: Text("First Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "First Page",
+              style: TextStyle(
+                  fontSize: 24, fontWeight: FontWeight.bold, color: Colors.red),
+            ),
+            ElevatedButton(
+                onPressed: () => {
+                      Navigator.pushNamed(context, SecondPage.routeName,
+                          arguments: pageArguments(
+                              "From First Page", "Hello Second Page"))
+                    },
+                child: Text("Go to Page 2")),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Fist Page",
-                  style: TextStyle(fontSize: 26, color: Colors.teal[300])),
-              ElevatedButton(
-                  onPressed: () => {Navigator.pushNamed(context, '/Second')},
-                  child: Text("Go to page 2")),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
 
 class SecondPage extends StatelessWidget {
   const SecondPage({Key? key}) : super(key: key);
+  static const routeName = "/SecondPage";
+
+  @override
+  Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)!.settings.arguments as pageArguments;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(args.title),
+        backgroundColor: Colors.green[800],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              args.message,
+              style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.green[900],
+                  fontWeight: FontWeight.bold),
+            ),
+            ElevatedButton(
+              onPressed: () => {Navigator.pop(context)},
+              child: Text(
+                "Go to Page 1",
+              ),
+              style: ElevatedButton.styleFrom(primary: Colors.red),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final result =
+                    await Navigator.pushNamed(context, ThirdPage.routeName);
+                ScaffoldMessenger.of(context)
+                  ..removeCurrentSnackBar()
+                  ..showSnackBar(SnackBar(content: Text('$result')));
+              },
+              child: Text("Go to Page 3"),
+              style: ElevatedButton.styleFrom(primary: Colors.blue),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  const ThirdPage({Key? key}) : super(key: key);
+  static const routeName = "/ThirdPage";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Second Page'),
+      appBar: AppBar(
+        title: Text("Third Page"),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Third Page",
+              style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.blue[900],
+                  fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => {Navigator.pop(context, "Yes.")},
+                  child: Text("YES."),
+                  style: ElevatedButton.styleFrom(primary: Colors.purple),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                  onPressed: () => {Navigator.pop(context, "No!!")},
+                  child: Text("NO!!"),
+                  style: ElevatedButton.styleFrom(primary: Colors.amber[900]),
+                ),
+              ],
+            )
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Second Page",
-                  style: TextStyle(fontSize: 26, color: Colors.green[900])),
-              ElevatedButton(
-                  onPressed: () => {Navigator.pushNamed(context, '/')},
-                  child: Text("Go to page 1")),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
