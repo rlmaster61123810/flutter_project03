1fd196e311d8a6385fc975452eb1f0e4a78b9398
The new begin <42103767+rlmaster61123810@users.noreply.github.com>
2021-09-08 12:36:44 +0700

    assets

--- lib/main.dart
+++ lib/main.dart
@@ -4,153 +4,183 @@ void main() {
   runApp(MyApp());
 }
 
-class pageArguments {
-  final String title;
-  final String message;
-  pageArguments(this.title, this.message);
-}
-
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.lightBlue,
       ),
-      // home: FirstPage(),
-      routes: {
-        '/': (context) => const FirstPage(),
-        // '/second': (context) => const SecondPage(),
-        SecondPage.routeName: (context) => const SecondPage(),
-        ThirdPage.routeName: (context) => const ThirdPage(),
-      },
+      home: MyHomePage(title: 'DOG RANK'),
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("First Page"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "First Page",
-              style: TextStyle(
-                  fontSize: 24, fontWeight: FontWeight.bold, color: Colors.red),
-            ),
-            ElevatedButton(
-                onPressed: () => {
-                      Navigator.pushNamed(context, SecondPage.routeName,
-                          arguments: pageArguments(
-                              "From First Page", "Hello Second Page"))
-                    },
-                child: Text("Go to Page 2")),
-          ],
-        ),
-      ),
-    );
-  }
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class SecondPage extends StatelessWidget {
-  const SecondPage({Key? key}) : super(key: key);
-  static const routeName = "/SecondPage";
-
+class _MyHomePageState extends State<MyHomePage> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
-    final args = ModalRoute.of(context)!.settings.arguments as pageArguments;
     return Scaffold(
       appBar: AppBar(
-        title: Text(args.title),
-        backgroundColor: Colors.green[800],
+        title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              args.message,
-              style: TextStyle(
-                  fontSize: 28,
-                  color: Colors.green[900],
-                  fontWeight: FontWeight.bold),
+      body: ListView(
+        padding: EdgeInsets.all(5),
+        children: [
+          Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/p01.jpg")),
+              title: Text("Dog Rank", style: TextStyle(color: Colors.white)),
+              subtitle: Text("class 1", style: TextStyle(color: Colors.white)),
+              trailing: Wrap(children: [
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+              ]),
+              tileColor: Colors.indigo[900],
             ),
-            ElevatedButton(
-              onPressed: () => {Navigator.pop(context)},
-              child: Text(
-                "Go to Page 1",
+          ),
+          Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/p02.png")),
+              title: Text("Dog Rank", style: TextStyle(color: Colors.white)),
+              subtitle: Text("class 2", style: TextStyle(color: Colors.white)),
+              trailing: Wrap(children: [
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+              ]),
+              tileColor: Colors.indigo[800],
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/p03.png")),
+              title: Text("Dog Rank", style: TextStyle(color: Colors.white)),
+              subtitle: Text("class 3", style: TextStyle(color: Colors.white)),
+              trailing: Wrap(children: [
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+              ]),
+              tileColor: Colors.indigo[700],
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/images/p04.png")),
+              title: Text("Dog Rank", style: TextStyle(color: Colors.white)),
+              subtitle: Text("class 4", style: TextStyle(color: Colors.white)),
+              trailing: Wrap(children: [
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+              ]),
+              tileColor: Colors.indigo[600],
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(
+                  "assets/images/p05.jpg",
+                ),
               ),
-              style: ElevatedButton.styleFrom(primary: Colors.red),
+              title: Text("Dog Rank", style: TextStyle(color: Colors.white)),
+              subtitle: Text("class 5", style: TextStyle(color: Colors.white)),
+              trailing: Wrap(children: [
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+              ]),
+              tileColor: Colors.indigo[600],
             ),
-            ElevatedButton(
-              onPressed: () async {
-                final result =
-                    await Navigator.pushNamed(context, ThirdPage.routeName);
-                ScaffoldMessenger.of(context)
-                  ..removeCurrentSnackBar()
-                  ..showSnackBar(SnackBar(content: Text('$result')));
-              },
-              child: Text("Go to Page 3"),
-              style: ElevatedButton.styleFrom(primary: Colors.blue),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ThirdPage extends StatelessWidget {
-  const ThirdPage({Key? key}) : super(key: key);
-  static const routeName = "/ThirdPage";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Third Page"),
-        backgroundColor: Colors.blue[900],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Third Page",
-              style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.blue[900],
-                  fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () => {Navigator.pop(context, "Yes.")},
-                  child: Text("YES."),
-                  style: ElevatedButton.styleFrom(primary: Colors.purple),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                ElevatedButton(
-                  onPressed: () => {Navigator.pop(context, "No!!")},
-                  child: Text("NO!!"),
-                  style: ElevatedButton.styleFrom(primary: Colors.amber[900]),
-                ),
-              ],
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
